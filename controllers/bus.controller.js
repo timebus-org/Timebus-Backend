7368a7985112b0d90bf3349fb44e3137884386f7
timebus-org <timebus.in@gmail.com>
2026-01-23 17:22:05 +0530

    Update bus.controller.js

--- controllers/bus.controller.js
+++ controllers/bus.controller.js
@@ -1,8 +1,21 @@
 const Bus = require("../models/Bus");
 
 exports.searchBuses = async (req, res) => {
-  const { from, to, date } = req.query;
+  try {
+    const { from, to, date } = req.query;
 
-  const buses = await Bus.find({ from, to, date });
-  res.json(buses);
+    const buses = await Bus.find({
+      from: { $regex: new RegExp(`^${from}$`, "i") },
+      to: { $regex: new RegExp(`^${to}$`, "i") },
+      date: date // date must match exactly, keep as is
+    });
+
+    console.log("Query:", req.query);
+    console.log("Buses found:", buses.length);
+
+    res.json(buses);
+  } catch (err) {
+    console.error("Search error:", err);
+    res.status(500).json([]);
+  }
 };
