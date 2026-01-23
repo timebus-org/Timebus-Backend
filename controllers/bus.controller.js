2428d23ca7cc6d2b5629d9e6cafd5fd50931a966
timebus-org <timebus.in@gmail.com>
2026-01-23 17:46:19 +0530

    Update bus.controller.js

--- controllers/bus.controller.js
+++ controllers/bus.controller.js
@@ -1,26 +1,22 @@
 const Bus = require("../models/Bus");
-
 exports.searchBuses = async (req, res) => {
   try {
     const { from, to, date } = req.query;
-
     // Trim inputs to remove extra spaces
     const fromTrimmed = from?.trim();
     const toTrimmed = to?.trim();
     const dateTrimmed = date?.trim();
-
     const buses = await Bus.find({
       from: { $regex: new RegExp(`^${fromTrimmed}$`, "i") },
       to: { $regex: new RegExp(`^${toTrimmed}$`, "i") },
       date: dateTrimmed
     });
-
     console.log("Query:", req.query);
     console.log("Buses found:", buses.length);
-
     res.json(buses);
   } catch (err) {
     console.error("Search error:", err);
     res.status(500).json([]);
   }
 };
+
