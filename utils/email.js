b5a95213a7425ea79a8e839c1b9379e76c1442ad
timebus-org <timebus.in@gmail.com>
2026-02-03 20:35:06 +0530

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -5,11 +5,12 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (subject, html) => {
   await resend.emails.send({
     from: "TimeBus <onboarding@resend.dev>",
-    to: "haribakk@gmail.com",
+    to: "timebus.in@gmail.com",
     subject,
     html,
   });
 };
 
 export default sendEmail;
+
 
